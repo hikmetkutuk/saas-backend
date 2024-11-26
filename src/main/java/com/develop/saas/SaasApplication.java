506727b118f23a2500a3735262e961517334bad5
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-11-26 14:27:31 +0300

    initial commit

---
+++ src/main/java/com/develop/saas/SaasApplication.java
@@ -0,0 +1,13 @@
+package com.develop.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasApplication.class, args);
+	}
+
+}
